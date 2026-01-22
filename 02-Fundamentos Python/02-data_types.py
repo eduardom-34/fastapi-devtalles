b16dba4a85b16be5b9cb8584c3597a2f7ce9b739
CesarLinux <eduardomelchor34@gmail.com>
2026-01-21 21:16:21 -0600

    Tipos de datos

---
+++ 02-Fundamentos Python/02-data_types.py
@@ -0,0 +1,23 @@
+
+
+#String
+name =  "Ricardo"
+last_name = "Cuellas"
+
+# Int
+number = 2000
+age = 29
+waste = 25
+
+# floats
+decimal = 20.42
+temp = 27.3
+
+# Boolean
+boolean = True # 1
+boolean_false = False # 0
+is_gamer = True
+
+
+
+
