7282455af63cf27a55a6778e655d9a835d1b5c31
CesarLinux <eduardomelchor34@gmail.com>
2026-01-25 10:32:13 -0600

    Sets

---
+++ 02-Fundamentos Python/08-sets.py
@@ -0,0 +1,13 @@
+
+# al igual que las diccionarios, van entre corchetes {}
+# Como sabe el interprete que se trata de un set y no un diccionario?
+# por que no necesita par ordenado de clave-valor
+# los sets no tienen indice, son desordenadas y NO permite duplicados
+
+my_set = {1,2,3,4,5,6,7,2,2,2,10,9,5,6}
+# print(my_set) # si hay duplicados, solo los imprime una vez
+
+usernames = {"cesar", "fernando", "devi", "fernando" }
+print(usernames)
+
+
