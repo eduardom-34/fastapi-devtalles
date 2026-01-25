258d750982c25466832ad9ec6310629e01bd7d86
CesarLinux <eduardomelchor34@gmail.com>
2026-01-25 10:26:08 -0600

    Tuplas

---
+++ 02-Fundamentos Python/07-tuples.py
@@ -0,0 +1,24 @@
+
+
+# las tuplas, al igual que las listas, son ordenada, es decir, itenen indice, empezando por cero
+# tambien permiten varios tipos de valores y permiten duplicadas, al igual que las listas
+# a diferencia de las listas que comienzan con [] las tuplas usa ()
+# a diferencia de las listas que son mutables, las tuplas no lo son
+
+my_tuple = (1,2,3,4, "Hola", True, False, 2, "hi", 3, 2)
+
+
+print(my_tuple)
+
+# que pasa si intentamos mutar la tupla?
+my_tuple[1] = 20
+# print(my_tuple) (da error)
+
+# las tuplas se usan para valores fijos, como dias de la semana
+
+week = ("lunes", "Martes", "Miercoles")
+
+
+
+
+
