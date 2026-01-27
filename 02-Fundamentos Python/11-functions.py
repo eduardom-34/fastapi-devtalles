87a433923ae5601c065c4b1ee8523f0f080135ba
CesarLinux <eduardomelchor34@gmail.com>
2026-01-27 10:04:27 -0600

    Funciones

---
+++ 02-Fundamentos Python/11-functions.py
@@ -0,0 +1,15 @@
+
+# Parametros
+def hello(greet = "Hola", name = "Invitado"):
+    print(f"{greet}, {name}")
+
+
+
+# Cuando llamamos a una funcion que necesita parametros, nosotros le mandamo argumentos
+hello("Hola", "Cesar")
+hello("Ciao", "Eduardo")
+hello()
+hello(name = "Teddy", greet = "Hello")
+
+
+
