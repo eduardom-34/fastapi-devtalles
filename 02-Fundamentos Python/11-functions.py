ba525b3828e99ab3ccf831f84e565834fd384b20
CesarLinux <eduardomelchor34@gmail.com>
2026-01-28 16:26:50 -0600

    Args y Kwargs

--- 02-Fundamentos Python/11-functions.py
+++ 02-Fundamentos Python/11-functions.py
@@ -1,15 +1,21 @@
 
-# Parametros
-def hello(greet = "Hola", name = "Invitado"):
-    print(f"{greet}, {name}")
+# # Parametros
+# def hello(greet = "Hola", name = "Invitado"):
+#     print(f"{greet}, {name}")
 
 
 
-# Cuando llamamos a una funcion que necesita parametros, nosotros le mandamo argumentos
-hello("Hola", "Cesar")
-hello("Ciao", "Eduardo")
-hello()
-hello(name = "Teddy", greet = "Hello")
+# # Cuando llamamos a una funcion que necesita parametros, nosotros le mandamo argumentos
+# hello("Hola", "Cesar")
+# hello("Ciao", "Eduardo")
+# hello()
+# hello(name = "Teddy", greet = "Hello")
 
 
 
+def big_function(*args, **kwargs):
+    print(args)
+    print(kwargs)
+    return kwargs
+    
+print(big_function(1,2,3,4,5, num1=77, nm2=30))
