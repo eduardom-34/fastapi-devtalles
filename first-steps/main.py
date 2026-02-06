33c8a73a4bd90b7949c44a1538ad7d5e9fe7f26d
CesarLinux <eduardomelchor34@gmail.com>
2026-02-06 09:38:01 -0600

    Get con datos

--- first-steps/main.py
+++ first-steps/main.py
@@ -2,6 +2,18 @@ from fastapi import FastAPI
 
 app = FastAPI(title="Mini Blog")
 
+
+BLOG_POST = [
+    {"id": 1, "title": "Hola desde FastAPI", "Content": "Este es mi primer post con FastAPI"},
+    {"id": 2, "title": "Mi segundo Post con FastAPI", "Content": "Este es mi segundo post con FastAPI"},
+    {"id": 3, "title": "Django vs FastAPI", "Content": "FastAPI es mas rapido por x razon"},
+]
+
 @app.get("/")
-def homre():
+def home():
     return {'message': "Bienvenidos a Mini Blog por Cesar"}
+
+
+@app.get("/posts")
+def list_posts():
+    return {"data": BLOG_POST}
